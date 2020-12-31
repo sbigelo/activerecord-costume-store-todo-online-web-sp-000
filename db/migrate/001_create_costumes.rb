be64b7834ff1c167def2615696e40fa3b74a5f93
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-31 19:35:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
         t.float :price
         t.string :size
         t.string :image_url
-        t.timestamps null: false
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
       end
     end
 
