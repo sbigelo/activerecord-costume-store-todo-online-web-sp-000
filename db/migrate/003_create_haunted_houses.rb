93a05263c2d1ff9bcbe1c03a9fd35d0a8ddec22b
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-31 20:37:51 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.string :friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_descrition
+
+      t.timestamps null: false
+    end
+  end
+end
+
+end
