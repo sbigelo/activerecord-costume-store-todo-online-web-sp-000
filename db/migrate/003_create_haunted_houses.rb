1d0c7fab664a4bac2100ba247849b2768060274c
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-31 20:42:24 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,17 +1,33 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouse < ActiveRecord::Migration[5.2]
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.string :friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
-      t.string :long_descrition
+#   def change
+#     create_table :haunted_houses do |t|
+#       t.string :name
+#       t.string :location
+#       t.string :theme
+#       t.integer :price
+#       t.string :friendly
+#       t.datetime :opening_date
+#       t.datetime :closing_date
+#       t.string :long_descrition
+#
+#       t.timestamps null: false
+#     end
+#   end
+# end
 
-      t.timestamps null: false
-    end
+def change
+  create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.float :price
+    t.string :theme
+    t.boolean :family_friendly
+    t.datetime :opening_date
+    t.datetime :closing_date
+    t.text :description
+
+    t.timestamps null: false
   end
+end
 end
